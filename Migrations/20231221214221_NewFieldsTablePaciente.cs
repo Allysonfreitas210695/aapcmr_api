7656f691a6c61893666941abf226ee55b3be639b
Allysonfreitas210695 <alisonfr83@gmail.com>
2023-12-21 18:45:07 -0300

    Atualizano tabela de paciente

---
+++ Migrations/20231221214221_NewFieldsTablePaciente.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_aapcmr.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFieldsTablePaciente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Celular",
+                table: "Pacientes",
+                type: "TEXT",
+                maxLength: 15,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelefoneFixo",
+                table: "Pacientes",
+                type: "TEXT",
+                maxLength: 15,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Celular",
+                table: "Pacientes");
+
+            migrationBuilder.DropColumn(
+                name: "TelefoneFixo",
+                table: "Pacientes");
+        }
+    }
+}
--- Repository/Paciente.cs
+++ Repository/Paciente.cs
@@ -13,6 +13,8 @@ namespace api_aapcmr.Repository
         public string CPF { get; set; }
         public string Sexo { get; set; }
         public bool Status { get; set; }
+        public string TelefoneFixo { get; set; }
+        public string Celular { get; set; }
         public long UsuarioId { get; set; }
         public Usuario Usuario { get; set; }
         public DateTime DataCriacao { get; set; }
@@ -21,7 +23,7 @@ namespace api_aapcmr.Repository
         public List<TratamentoPaciente> TratamentoPacientes { get; set; }
         public long? SituacaoHabitacionalId { get; set; }
         public SituacaoHabitacional SituacaoHabitacional { get; set; }
-         public List<ComposicaoFamiliar> ComposicaoFamiliares { get; set; }
+        public List<ComposicaoFamiliar> ComposicaoFamiliares { get; set; }
 
         public static void ConfiguraModelo(ModelBuilder modelBuilder)
         {
@@ -36,9 +38,10 @@ namespace api_aapcmr.Repository
                 etd.Property(c => c.DataNascimento).HasColumnType("datetime").IsRequired();
                 etd.Property(c => c.SUSNumero).HasMaxLength(15).IsRequired();
                 etd.Property(c => c.CPF).HasMaxLength(15).IsRequired();
+                etd.Property(c => c.TelefoneFixo).HasMaxLength(15);
+                etd.Property(c => c.Celular).HasMaxLength(15);
                 etd.Property(c => c.DataCriacao).HasColumnType("datetime").HasDefaultValueSql("GETDATE()");
                 etd.Property(c => c.DataAtualizacao).HasColumnType("datetime").HasDefaultValueSql("GETDATE()");
-                //fazer o relacioanmento
                 etd.HasOne(c => c.Usuario).WithMany(u => u.Pacientes).HasForeignKey(x => x.UsuarioId).OnDelete(DeleteBehavior.SetNull);
                 etd.HasOne(c => c.SituacaoHabitacional).WithOne(sh => sh.Paciente).HasForeignKey<Paciente>(c => c.SituacaoHabitacionalId).OnDelete(DeleteBehavior.NoAction);
             });
